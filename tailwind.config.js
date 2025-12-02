fccd7e66bfcd7e35ca25d22a62412fd67781d5b5
Philipp <50392861-thetaranov@users.noreply.replit.com>
2025-12-02 03:22:38 +0000

    Clean config fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,11 @@
 export default {
   content: [
     "./index.html",
-    "./**/*.{js,ts,jsx,tsx}",
+    "./*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
