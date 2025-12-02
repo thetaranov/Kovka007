b9838a02c8758e79804c2ddfb66dce2d7e4b4028
Philipp <50392861-thetaranov@users.noreply.replit.com>
2025-12-02 03:02:58 +0000

    Clean vite config to fix build

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "tailwindcss";
-import autoprefixer from "autoprefixer";
 
 export default defineConfig({
   plugins: [react()],
-  css: {
-    postcss: {
-      plugins: [
-        tailwindcss({
-          content: ["./index.html", "./**/*.{js,ts,jsx,tsx}"],
-          theme: {
-            extend: {},
-          },
-          plugins: [],
-        }),
-        autoprefixer(),
-      ],
-    },
-  },
   server: {
     host: "0.0.0.0",
     port: 5000,
