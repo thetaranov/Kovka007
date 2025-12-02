3981c80a67a066047e516edc88f93243392c8c7f
Philipp <50392861-thetaranov@users.noreply.replit.com>
2025-12-02 03:12:29 +0000

    Standard Tailwind config

--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,16 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "tailwindcss";
-import autoprefixer from "autoprefixer";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  css: {
-    postcss: {
-      plugins: [
-        tailwindcss({
-          // Конфигурация Tailwind прямо здесь
-          content: ["./index.html", "./**/*.{js,ts,jsx,tsx}"],
-          theme: {
-            extend: {},
-          },
-          plugins: [],
-        }),
-        autoprefixer(),
-      ],
-    },
-  },
   server: {
-    host: "0.0.0.0",
+    host: '0.0.0.0',
     port: 5000,
     hmr: {
       clientPort: 5000,
     },
   },
   build: {
-    outDir: "dist",
+    outDir: 'dist',
   },
 });
