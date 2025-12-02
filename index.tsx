b994e190c37668b687983eae5418b4349d2a60b2
Philipp <50392861-thetaranov@users.noreply.replit.com>
2025-12-02 03:45:02 +0000

    Restore working version with CDN

--- index.tsx
+++ index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css"; // <--- ВАЖНО
+// import "./index.css";  <-- УДАЛЕНО
 
 const rootElement = document.getElementById("root");
 if (!rootElement) {
