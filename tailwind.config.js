bbb48ae64cc92f0d79e2b69ce312e2441ef51fb9
Philipp <50392861-thetaranov@users.noreply.replit.com>
2025-12-02 02:28:05 +0000

    Move PostCSS config to Vite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
-    "./*.{js,ts,jsx,tsx}", // Для файлов в корне
   ],
   theme: {
     extend: {},
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,23 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
 
 export default defineConfig({
   plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [tailwindcss(), autoprefixer()],
+    },
+  },
   server: {
-    host: '0.0.0.0',
+    host: "0.0.0.0",
     port: 5000,
     hmr: {
       clientPort: 5000,
     },
   },
   build: {
-    outDir: 'dist',
+    outDir: "dist",
   },
 });
