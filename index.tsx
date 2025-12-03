e4a8a16f6a364a7294504c24b78b8856aa455078
thetaranov <philipp.taranov@gmail.com>
2025-12-03 04:14:14 +0000

    update

--- index.tsx
+++ index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-// import "./index.css";  <-- УДАЛЕНО
 
 const rootElement = document.getElementById("root");
 if (!rootElement) {
